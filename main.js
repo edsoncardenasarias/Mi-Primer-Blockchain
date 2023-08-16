c150f3207aeb94e4949e5ffc1f6ab08dd281e063
Edson Cárdenas Arias <72884555+edsoncardenasarias@users.noreply.github.com>
2023-08-16 09:44:42 -0500

    Recompensa Minería & Transacciones

--- main.js
+++ main.js
@@ -1,52 +1,111 @@
+//Utilizamos la libreria crypto-js para realizar el calculo del HASH de los parametros que indiquemos en el constructor del block.
 const SHA256 = require('crypto-js/sha256')
+
+//Añadimos una nueva clase para añadir transacciones en lugar del data.
+class Transaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
 
 //Primero de todo creamos nuestro bloque
 class Block {
     //Hacemos un constructor con las variables que comportan el bloque.
-    constructor(index, timestamp, data, previousHash = '') {
-            //Indice del bloque
-            this.index = index;
-            //Tiempo de la transacción
-            this.timestamp = timestamp;
-            //Datos del bloque como pueden ser dirección de las wallets, cantidad de crypto...
-            this.data = data;
-            //El Hash del anterior bloque.
-            this.previousHash = previousHash;
-            //Hash del actual bloque
+    //**Cambiamos data por transactions */
+    constructor(timestamp, transactions, previousHash = '') {
+        //Tiempo de la transacción
+        this.timestamp = timestamp;
+        //Datos del bloque como pueden ser dirección de las wallets, cantidad de crypto...
+        this.transactions = transactions;
+        //El Hash del anterior bloque.
+        this.previousHash = previousHash;
+        //Hash del actual bloque
+        this.hash = this.calculateHash();
+        //Añadimos Nonce a nuestro constructor **
+        this.nonce = 0;
+    }
+
+    //Añadimos el nonce al calculo del hash
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+    }
+
+    //Implementación proof of work con la función de minado del bloque.
+    mineBlock(difficulty) {
+        //Mientas no se cumpla la dificultad (en función de la dificultad indicada) estará realizando el calculo del hash.
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
             this.hash = this.calculateHash();
         }
-        //Función para calcular el Hash del bloque
-        //Un hash es el resultado de una función hash, la cual es una operación criptográfica que genera identificadores únicos 
-        //e irrepetibles a partir de una información dada. Los hashes son una pieza clave en la tecnología blockchain 
-        //y tiene una amplia utilidad.
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        console.log("Block mined: " + this.hash);
     }
 }
 
-//Creo mi blockchain
+//Creamos nuestra blockchain
 class Blockchain {
+
     constructor() {
-            this.chain = [this.createGenesisBlock()];
-        }
-        //Función para crear nuestro Bloque Genesis. Se llama bloque genesis al primer bloque de cada blockchain.
+        this.chain = [this.createGenesisBlock()];
+        //***Añadimos la variable de dificultad de minado del bloque en nuestro constructor.
+        this.difficulty = 4;
+        //Añadimos a nuestro constructor un array de transacciones pendientes.
+        this.pendingTransactions = [];
+        //Tambien añadimos un recompensa por el minado del bloque.
+        this.miningReward = 500;
+    }
+
+    //Función para crear nuestro Bloque Genesis. Se llama bloque genesis al primer bloque de cada blockchain.
     createGenesisBlock() {
-            return new Block(0, "12/04/2022", "Genesis Block", "0");
-        }
-        //Función para obtener el anterior bloque
+        return new Block(0, "01/01/2022", "Genesis Block", "0");
+    }
+
+    //Función para obtener el anterior bloque
     getLatestBlock() {
-            return this.chain[this.chain.length - 1];
+        return this.chain[this.chain.length - 1];
+    }
+
+    //Quitamos el AddBlock y lo cambiamos por la funcion de minado de transacciones pendientes, que la llamamos...miningPendingTransactions.
+    minedPendingTransactions(miningRewardAddress) {
+        //Definimos la creacion de un nuevo bloque.
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        //Le pasamos la dificultad al minado del bloque.
+        block.mineBlock(this.difficulty);
+
+        //Añadimos el bloque a la cadena
+        console.log('Bloque se ha minado correctamente');
+        this.chain.push(block);
+
+        //Cuando se ha minado la transaccion se crea otra transaccion para enviar el reward(premio) a la wallet del minero.
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    //Función para crear una transacción.
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    //Función para obtener el balance de una dirección.
+    getBalanceOfAddress(address) {
+        let balance = 0;
+
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (trans.fromAddress === address) {
+                    balance -= trans.amount;
+                }
+                if (trans.toAddress === address) {
+                    balance += trans.amount;
+                }
+            }
         }
-        //Función para añadir un nuevo bloque
-    addBlock(newBlock) {
-            //Primero debemos de obtener el anterior bloque
-            newBlock.previousHash = this.getLatestBlock().hash;
-            //Calcular el hash del actual bloque
-            newBlock.hash = newBlock.calculateHash();
-            //Añadir el bloque a la blockchain.
-            this.chain.push(newBlock);
-        }
-        //Función para comprobar si nuestra blockchain es valida.
+        return balance;
+    }
+
+    //Función para comprobar si nuestra blockchain es valida.
     isChainValid() {
         for (let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
@@ -66,10 +125,20 @@ class Blockchain {
 
 //Definimos el nombre de nuestra Blockchain
 let JJACoin = new Blockchain();
-//Añadimos nuevos bloques a nuestra Blockchain
-JJACoin.addBlock(new Block(1, "12/02/2022", { amount: 100 }));
-JJACoin.addBlock(new Block(2, "13/03/2022", { amount: 200 }));
-JJACoin.addBlock(new Block(3, "21/05/2022", { amount: 300 }));
-JJACoin.addBlock(new Block(4, "15/06/2022", { amount: 400 }));
-//Obtenemos la información de los bloques de nuestra blockchain
-console.log(JSON.stringify(JJACoin, null, 4));
+
+//Creamos nuevas transacciones de envio de dinero a diferentes wallets para luego comenzar el minado de los bloques de cada transaccion.
+JJACoin.createTransaction(new Transaction('Address 1', 'Address 2', 50));
+JJACoin.createTransaction(new Transaction('Address 2', 'Address 3', 450));
+
+//Comenzar el minado.
+console.log('\n Comenzando el minado....')
+JJACoin.minedPendingTransactions('Ariana-Address');
+//Obtener el balance del minero.
+console.log('\nEl Balance de Ariana es', JJACoin.getBalanceOfAddress('Ariana-Address'));
+
+//Minamos otro bloque para obtener la recompensa del minado que hemos establecido a partir del segundo bloque.
+console.log('\n Comenzando el minado otra vez....')
+JJACoin.minedPendingTransactions('Ariana-Address');
+
+//Obtener el balance final.
+console.log('\nEl Balance de Ariana es', JJACoin.getBalanceOfAddress('Ariana-Address'));
